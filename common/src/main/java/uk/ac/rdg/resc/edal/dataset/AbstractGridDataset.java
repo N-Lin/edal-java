6f1b07854f172883db09c64e55fdb8cf3391d414
N-Lin <n.lin@reading.ac.uk>
2014-09-23 11:19:52 +0100

    Dummy code of HovmoellerPosition, HovmoellerDomain, and HovmoellerFeature. new unfinished method extractHovmollerFeature in the class AbstractGridDataset. Only demo the logic of ideas. Not proper Java code.

--- common/src/main/java/uk/ac/rdg/resc/edal/dataset/AbstractGridDataset.java
+++ common/src/main/java/uk/ac/rdg/resc/edal/dataset/AbstractGridDataset.java
@@ -69,6 +69,7 @@ import uk.ac.rdg.resc.edal.feature.ProfileFeature;
 import uk.ac.rdg.resc.edal.feature.TrajectoryFeature;
 import uk.ac.rdg.resc.edal.geometry.BoundingBox;
 import uk.ac.rdg.resc.edal.geometry.BoundingBoxImpl;
+import uk.ac.rdg.resc.edal.geometry.LineString;
 import uk.ac.rdg.resc.edal.grid.GridCell2D;
 import uk.ac.rdg.resc.edal.grid.HorizontalGrid;
 import uk.ac.rdg.resc.edal.grid.TimeAxis;
